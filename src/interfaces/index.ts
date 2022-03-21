c6c6689e933f981c5b2170616aec102141061e5b
rmonteroh <rmonteroh90@gmail.com>
2022-03-21 16:04:03 -0500

    Updates

--- src/interfaces/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './signature-image-url.interface';
--- src/interfaces/signature-image-url.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface ISignatureImagesUrl {
-  logo: string;
-  facebook: string,
-  linkedIn: string,
-  instagram: string,
-  twitter: string,
-  youtube: string,
-  github: string,
-}
